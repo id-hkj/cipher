47dd9fa31f8ebb1757e649ac4c5d9b5e29126666
id-hkj <83733473+id-hkj@users.noreply.github.com>
2024-04-28 17:53:26 +0100

    Adding what exists so far

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+
+namespace Cipher
+{
+    class Program
+    {
+        static string[] Parameter_Pass(string[] els)
+        {
+            var new_arr = new string[els.Length - 1];
+            for (int i = 0; i < new_arr.Length; i++)
+            {
+                new_arr[i] = els[i + 1];
+            }
+            return new_arr;
+        }
+
+        static void Data_Decide(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid Parameters");
+                return;
+            }
+            switch (args[0])
+            {
+                case "clean" when args.Length >= 2:
+                    if (args.Length == 2) { Data.corpus_clean(args[1], args[1]); }
+                    else { Data.corpus_clean(args[1], args[2]); }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid Parameters");
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "-h":
+                case "--help":
+                    Console.WriteLine("The following commands are available. For more information on what each one does, "
+                                    + "run the help command with that keyword in front (e.g. cipher data -h)");
+                    Console.WriteLine();
+                    Console.WriteLine("data - Some Corpus and Fittness functions");
+                    Console.WriteLine("mono - Monalphabetic substitution cipher solver/encoders");
+                    Console.WriteLine("poly - Polyalphabetic substitution cipher solver/encoders");
+                    Console.WriteLine("transpose - Transposition cipher solver/encoders");
+                    Console.WriteLine("grid - solver/encoder for ciphers based on grids");
+                    Console.WriteLine("matrix - solver/encoder for ciphers based on matrices");
+                    Console.WriteLine("stream - Stream ciphers solver/encoder");
+                    Console.WriteLine("code");
+                    Console.WriteLine("proto - Protomechanical ciphers solver/encoder");
+                    Console.WriteLine("other - Miscelanious ciphers");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("As of writing, only data has been implemented.");
+                    break;
+                case "data":
+                    Data_Decide(Parameter_Pass(args));
+                    break;
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+            }
+        }
+    }
+}
---
+++ data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Cipher
+{
+    public class Data
+    {
+        public static void corpus_clean(string read_file, string out_file)
+        {
+            read_file = Path.GetFullPath(read_file);
+            out_file = Path.GetFullPath(out_file);
+
+            if (!File.Exists(read_file)) {
+                Console.WriteLine("ERROR: Invalid read file");
+                return;
+            }
+
+            var sr = new StreamReader(read_file);
+            string total = sr.ReadToEnd();
+            string final = "";
+            
+            Console.WriteLine("Opened file");
+            Console.WriteLine("Starting clean...");
+            
+            foreach (char character in total)
+            {
+                switch (Convert.ToInt32(character))
+                {
+                    case 32:
+                    case 10:
+                        final += " ";
+                        break;
+                    case int c when (c >= 65 & c <= 90):
+                        final += character;
+                        break;
+                    case int c when (c >= 97 & c < 122):
+                        final += Convert.ToChar(Convert.ToInt32(character) - 32);
+                        break;
+                }
+            }
+            Console.WriteLine(final);
+
+            Console.WriteLine("Clean finished.");
+            Console.WriteLine("Writing result to file");
+
+            using (StreamWriter sw = new StreamWriter(out_file))
+            {
+                sw.WriteLine(final);
+            }
+            Console.WriteLine("Complete.");
+        }
+    }
+}
