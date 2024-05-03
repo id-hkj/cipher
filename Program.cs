aac041b1875950a3d414f1dadabaefe4e1e6eba0
id-hkj <83733473+id-hkj@users.noreply.github.com>
2024-05-03 21:38:40 +0100

    Update Program.cs
    It is now, purely, selection.
    This file looks at the statement provided in the command line, and figures out what command to run, based on it.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.Design;
 using System.IO;
 
@@ -18,16 +18,178 @@ namespace Cipher
 
         static void Data_Decide(string[] args)
         {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Invalid Parameters");
-                return;
-            }
             switch (args[0])
             {
                 case "clean" when args.Length >= 2:
-                    if (args.Length == 2) { Data.corpus_clean(args[1], args[1]); }
-                    else { Data.corpus_clean(args[1], args[2]); }
+                    if (args.Length == 2) { Data.Corpus_Clean(args[1], args[1]); }
+                    else { Data.Corpus_Clean(args[1], args[2]); }
+                    break;
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Mono_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Poly_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Trans_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Grid_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Matrix_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Stream_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Code_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Other_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Proto_Decide(string[] args)
+        {
+            switch (args[0])
+            {
+                default:
+                    Console.WriteLine("Invalid Parameters");
+                    break;
+
+            }
+        }
+
+        static void Help_Decide(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Help.Summary();
+            }
+            switch (args[0])
+            {
+                case "all":
+                    Help.All_Commands();
+                    break;
+                case "-d":
+                case "--data":
+                case "data":
+                    Help.Data();
+                    break;
+                case "-m":
+                case "--mono":
+                case "mono":
+                    Help.Mono();
+                    break;
+                case "-p":
+                case "--poly":
+                case "poly":
+                    Help.Poly();
+                    break;
+                case "-t":
+                case "--transpose":
+                case "transpose":
+                    Help.Trans();
+                    break;
+                case "-g":
+                case "--grid":
+                case "grid":
+                    Help.Grid();
+                    break;
+                case "-M":
+                case "--matrix":
+                case "matrix":
+                    Help.Matrix();
+                    break;
+                case "-s":
+                case "--stream":
+                case "stream":
+                    Help.Streams();
+                    break;
+                case "-c":
+                case "--code":
+                case "code":
+                    Help.Code();
+                    break;
+                case "-o":
+                case "--other":
+                case "other":
+                    Help.Other();
+                    break;
+                case "-P":
+                case "--proto":
+                case "proto":
+                    Help.Proto();
                     break;
                 default:
                     Console.WriteLine("Invalid Parameters");
@@ -44,33 +206,69 @@ namespace Cipher
                 return;
             }
 
+            string[] Pass_Params = Parameter_Pass(args);
+
+            // First switch is for functions that don't require a parameter
             switch (args[0])
             {
                 case "-h":
                 case "--help":
-                    Console.WriteLine("TUsage: cipher [options...]");
-                    Console.WriteLine("data - Some Corpus and Fittness functions");
-                    Console.WriteLine("mono - Monalphabetic substitution cipher solver/encoders");
-                    Console.WriteLine("poly - Polyalphabetic substitution cipher solver/encoders");
-                    Console.WriteLine("transpose - Transposition cipher solver/encoders");
-                    Console.WriteLine("grid - solver/encoder for ciphers based on grids");
-                    Console.WriteLine("matrix - solver/encoder for ciphers based on matrices");
-                    Console.WriteLine("stream - Stream ciphers solver/encoder");
-                    Console.WriteLine("code");
-                    Console.WriteLine("proto - Protomechanical ciphers solver/encoder");
-                    Console.WriteLine("other - Miscelanious ciphers");
-                    Console.WriteLine();
-                    Console.WriteLine("This is not the full help, this menu is stripped into categories.");
-                    Console.WriteLine("Use " + '"' + "--help category + '"' + " to get an overview of all categories.");
-                    Console.WriteLine("For all options use the manual or " + '"' " "--help all" + '"' + ". (not yet implemented)");
-                    Console.WriteLine();
-                    Console.WriteLine("As of writing, only data has been implemented.");
+                    Help_Decide(Pass_Params);
+                    return;
+                default:
+                    if (Pass_Params.Length == 0)
+                    {
+                        Console.WriteLine("Invalid Parameters");
+                        return;
+                    }
                     break;
-                case "data":
-                    Data_Decide(Parameter_Pass(args));
+            }
+
+            switch (args[0])
+            {
+                case "-d":
+                case "--data":
+                    Data_Decide(Pass_Params);
+                    break;
+                case "-m":
+                case "--mono":
+                    Mono_Decide(Pass_Params);
+                    break;
+                case "-p":
+                case "--poly":
+                    Poly_Decide(Pass_Params);
+                    break;
+                case "-t":
+                case "--transpose":
+                    Trans_Decide(Pass_Params);
+                    break;
+                case "-g":
+                case "--grid":
+                    Grid_Decide(Pass_Params);
+                    break;
+                case "-M":
+                case "--matrix":
+                    Matrix_Decide(Pass_Params);
+                    break;
+                case "-s":
+                case "--stream":
+                    Stream_Decide(Pass_Params);
+                    break;
+                case "-c":
+                case "--code":
+                    Code_Decide(Pass_Params);
+                    break;
+                case "-o":
+                case "--other":
+                    Other_Decide(Pass_Params);
+                    break;
+                case "-P":
+                case "--proto":
+                    Proto_Decide(Pass_Params);
                     break;
                 default:
-                    Console.WriteLine("Invalid Parameters");
+                    Console.WriteLine("cipher: option " + args[0] + ": is unknown");
+                    Console.WriteLine("cipher: try 'cipher --help' for more information");
                     break;
             }
         }
